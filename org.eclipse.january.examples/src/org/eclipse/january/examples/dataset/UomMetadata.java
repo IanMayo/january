b2f1d4173b12dc3ff2ffe7ae40bc4a4d62d03d17
Ian Mayo <ian@planetmayo.com>
2017-03-17 11:17:49 +0000

    introduce UoM metadata class, as a step towards making IDataset units-aware.

---
+++ org.eclipse.january.examples/src/org/eclipse/january/examples/dataset/UomMetadata.java
@@ -0,0 +1,25 @@
+package org.eclipse.january.examples.dataset;
+
+import org.eclipse.january.metadata.MetadataType;
+import org.unitsofmeasurement.unit.Unit;
+
+public class UomMetadata implements MetadataType {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private final Unit<?> unit;
+
+	public UomMetadata(Unit<?> unit) {
+		this.unit = unit;
+	}
+
+	public UomMetadata clone() {
+		return new UomMetadata(unit);
+	}
+
+	public Unit<?> getUnit() {
+		return unit;
+	}
+}
